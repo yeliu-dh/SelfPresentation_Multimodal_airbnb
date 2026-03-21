c93b7489dd327df712953db1f7bcd1cc5a24843c
Ye_LIU <liuyefr@126.com>
2026-03-21 11:31:12 +0100

    zsc def

--- utils/zsc.py
+++ utils/zsc.py
@@ -5,8 +5,6 @@ from tqdm import tqdm
 from datetime import datetime
 from transformers import pipeline
 import torch
-import torch._dynamo
-torch._dynamo.config.suppress_errors = True
 
 """
 
@@ -24,107 +22,23 @@ results = classifier_bge(sequences, candidate_labels=labels, multi_label=True)
 
 
 """
-
-
-
-def run_zsc(
-    path_df_unique,
-    dict_items=None,
-    model_name="MoritzLaurer/bge-m3-zeroshot-v2.0",#"MoritzLaurer/bge-m3-zeroshot-v2.0", #"tasksource/ModernBERT-large-nli",#par défaut
-    by_lang=True,
-    outpath_zsc_result=None, 
-    save_interval=1000,# save every 
-    log=True):
-    
-    """
-        无论如何都要输入原df_unique用于筛选除了中间结果中还有多少todo！
-        自己定义输出路径：有中间结果，则更新原路径文件
-        
-        dict_items无输入时取默认；
-        
-        models :
-        "MoritzLaurer/bge-m3-zeroshot-v2.0"
-        "tasksource/ModernBERT-large-nli"
-        
-        outpath_zsc_result=output_zsc/model_name/listings_paris2406_items.csv
-        
-        返回df_zsc :id, host_about,lang, labels
-        
-    """
-       
-    if not dict_items:     
-        labels_en=[
-            'open to different cultures', 'cosmopolitan','international view', 'cultural exchange',
-            'personal life', 'life experiences', 'divers interests', 'hobbies', 'enjoy life',
-            'meet new people', 'welcoming', 'friendly', 'sociable', 'interpersonal interaction',
-            'thoughtful service', 'attentive to needs', 'willing to help', 'responsive',
-            'fan of Airbnb', 'Airbnb community','love Airbnb', 'travel with Airbnb'
-        ]
-        labels_fr=[
-                'ouvert aux différentes cultures', 'cosmopolite','vue internationale', 'échange culturel',
-                'vie personnelle', 'parcours personnel', 'loisirs', 'passions', 'aimer la vie',
-            'rencontrer de nouvelles personnes', 'accueillant', 'amical','sociable', 'interaction interpersonnelle',
-            'rendre service','attentif aux besoins', 'prêt à aider', 'réactif',
-            "adepte d'Airbnb",'communauté Airbnb', 'aime Airbnb', 'voyager par Airbnb'
-            ]
-        dict_items={'en':labels_en,
-                    'fr':labels_fr}
-        dict_fr2en=dict(zip(labels_fr, labels_en))
-        if len(labels_en)!=len(labels_fr):
-            print(f"[CHECK] labels fr match labels en !")
-    
-    ## input    
-    print("load df_unique & previous result".center(100,'-'))    
-    df_unique=pd.read_csv(path_df_unique)
-
-    # 已有结果，接着做没有处理过的：
-    if not os.path.exists(outpath_zsc_result):
-        print(f"[INFO] first zsc!")
-        os.makedirs(os.path.dirname(outpath_zsc_result), exist_ok=True)
-        df_done = pd.DataFrame()
-        df_todo=df_unique.copy()
-        
-    else:
-        df_done = pd.read_csv(outpath_zsc_result)
-        done_ids = set(df_done["id"])
-        df_todo = df_unique[~df_unique["id"].isin(done_ids)]
-        print(f"[CHECK by id] {len(done_ids)} rows DONE,\n"
-            f" {len(df_todo)} rows TO DO.")
-
-    # df to zsc
-    df=df_todo.copy()           
-
-    # model
-    print("load model".center(100,'-'))
-    start_time_model=time.time()
-    device=0 if torch.cuda.is_available() else -1
-    classifier = pipeline(
-        "zero-shot-classification",
-        model=model_name,
-        device=device
-    )
-    end_time_model=time.time()
-
-    print(f"[MODEL] {model_name} loaded in {end_time_model-start_time_model:.2f} sec !")
-
-    
-    ## zsc 
-    print("df_todo ==ZSC==> results_list".center(100,'-'))   
-    
-    def classify_row(row, by_lang):
+def zsc_text(row, classifier, by_lang, dict_items):
+        # host_about
         text = row["host_about"]            
-        lang= row["lang"]
+        
+        # labels
+        if by_lang :
+            # 若分语言，寻找对应语言的items，若没有对应语言的items，默认en items
+            lang= row["lang"]
+            candidate_labels=dict_items.get(lang, dict_items['en'])
+        else :
+            candidate_labels=dict_items['en']
+
+        # zsc
+        dict_fr2en=dict(zip(dict_items["fr"], dict_items["en"]))
         
         try:
-            # labels
-            if by_lang :
-                candidate_labels=dict_items[lang]
-            else :
-                candidate_labels=dict_items['en']
-
-            #zsc
-            res = classifier(text, candidate_labels, multi_label=True)#***
-            
+            res = classifier(text, candidate_labels, multi_label=True)#*** 
             # labels fr2en:
             if by_lang and lang == "fr":
                 labels_en = [dict_fr2en[label] for label in res["labels"]]
@@ -133,8 +47,8 @@ def run_zsc(
             dict_scores = dict(zip(labels_en, res["scores"]))
 
             return  {
-                    "id": row["id"],
-                    "text":text,
+                    "host_id": row["id"],
+                    "host_about":text,
                     # "lang": lang,
                     **dict_scores
                 }
@@ -143,59 +57,116 @@ def run_zsc(
             # 报错则将所有labels en初始化为nan
             all_en_labels = dict_fr2en.values() if lang == "fr" else dict_items["en"]
             return {
-                "id": row["id"],
-                "text":text,
+                "host_id": row["id"],
+                "host_about":text,
                 # "lang": lang,
                 **{label: np.nan for label in all_en_labels}
             }
+            
+
+
+def run_zsc(
+    df_input,
+    model_name="MoritzLaurer/bge-m3-zeroshot-v2.0",#"MoritzLaurer/bge-m3-zeroshot-v2.0", #"tasksource/ModernBERT-large-nli",#par défaut
+    by_lang=True,
+    path_results=None, 
+    save_interval=1000, 
+    ):
     
-    results_list = []
-    start_time = time.time()
-    if by_lang:    
-        print(f"[INFO] zsc by lang ! ")
-    for idx, row in tqdm(df.iterrows(), total=len(df), desc="ZSC"):
-        results_list.append(classify_row(row, by_lang=by_lang))
+    """
+    INPUT : df_processed
+
+    OUTPUT : 
+        results: csv : 
+            host_id:int, host_about:str, items:int
+        df_zsced=df_processed.merge(results_zsc)
         
-        # save_interval
+    """
+    
+    # ---input: df_unique--- 
+    df=df_input.copy()
+    df_unique=df.dropna(subset="host_about").drop_duplicates(subset="host_about")
+    print(f"[info] df: {len(df)}; df_unique on 'host_about': {len(df_unique)}\n")
+    
+    
+    # ---dict_items---
+    labels_en=[
+        'open to different cultures', 'cosmopolitan','international view', 'cultural exchange',
+        'personal life', 'life experiences', 'divers interests', 'hobbies', 'enjoy life',
+        'meet new people', 'welcoming', 'friendly', 'sociable', 'interpersonal interaction',
+        'thoughtful service', 'attentive to needs', 'willing to help', 'responsive',
+        'fan of Airbnb', 'Airbnb community','love Airbnb', 'travel with Airbnb'
+    ]
+    labels_fr=[
+            'ouvert aux différentes cultures', 'cosmopolite','vue internationale', 'échange culturel',
+            'vie personnelle', 'parcours personnel', 'loisirs', 'passions', 'aimer la vie',
+        'rencontrer de nouvelles personnes', 'accueillant', 'amical','sociable', 'interaction interpersonnelle',
+        'rendre service','attentif aux besoins', 'prêt à aider', 'réactif',
+        "adepte d'Airbnb",'communauté Airbnb', 'aime Airbnb', 'voyager par Airbnb'
+        ]
+    dict_items={'en':labels_en,
+                'fr':labels_fr}
+    # dict_fr2en=dict(zip(dict_items["fr"], dict_items["en"]))
+    
+    if len(labels_en)!=len(labels_fr):
+        print(f"[CHECK] labels fr match labels en !")
+    
+    # ---IO:no repetition---
+    # NB. host_id, id都是唯一的，但是host_about不是！
+    
+    if os.path.exists(path_results):
+        results_df=pd.read_csv(path_results)
+        processed=set(results_df['host_id'])
+        pending_df=df_unique[~df_unique['host_id'].isin(processed)]  
+        print(f"[info] {len(processed)} rows already zsced; {len(pending_df)} rows to zsc!\n")
+
+    else :
+        pending_df=df_unique.copy()#input
+        results_df=pd.DataFrame()#output
+        print(f"0 rows zsc!")
+    
+    
+    
+    # ---model---
+    print(f"loading model...")
+    
+    device=0 if torch.cuda.is_available() else -1
+    classifier = pipeline(
+        "zero-shot-classification",
+        model=model_name,
+        device=device
+    )
+
+    ## zsc 
+    results= []
+    start_time = time.time()
+    if by_lang:
+        print(f"[info] zsc by lang !")
+
+    for idx, row in tqdm(pending_df.iterrows(), total=len(pending_df), desc="ZSC text..."):
+        result=zsc_text(row, classifier, by_lang, dict_items)
+        results.append(result)
+        
+        # ---save every----
         if (idx + 1) % save_interval == 0:
-            ## 每次要和df_done合并保存，不然结果中只有上一轮论更新的，丢失上一次之前的！
-            df_zsc = pd.concat([df_done, pd.DataFrame(results_list)], ignore_index=True)
-            df_zsc.to_csv(outpath_zsc_result, index=False)
-            print(f"✔ [SAVE] checkpoint {len(df_zsc)} / {len(df_unique)} saved to {outpath_zsc_result}!\n") 
+            results_df = pd.concat([results_df, pd.DataFrame(results)], ignore_index=True)
+            results_df.to_csv(path_results, index=False)
+            results=[]# 重置
+            # results_df不断增加并保存，results储存每save_interval条结果
+            print(f"[INTERVAL SAVE] {len(results_df)} / {len(df_unique)} rows zsced and saved!\n")            
     
     ## 最后一次保存!!!
-    if results_list:
-        # 合并done+todo :
-        df_zsc = pd.concat([df_done, pd.DataFrame(results_list)], ignore_index=True)
-        # save :有结果的更新原结果
-        df_zsc.to_csv(outpath_zsc_result, index=False)
-        print(f"✅ [FINAL SAVE] {len(df_zsc)} rows saved to {outpath_zsc_result}!")
+    if results:
+        results_df = pd.concat([results_df, pd.DataFrame(results)], ignore_index=True)
+        results_df.to_csv(path_results, index=False)
+
+        print(f"✅ [FINAL SAVE] {len(results_df)}/{len(df_unique)} rows saved to {path_results}!\n")
     
     end_time = time.time()
-    print(f"\n ⭐ [SUCCESS] ZSC sur {len(df)} textes avec {len(dict_items['en'])} EN labels/{len(dict_items['fr'])} FR labels \n"
+    print(f"\n[DONE] ZSC sur {len(df)} textes avec {len(dict_items['en'])} EN labels/{len(dict_items['fr'])} FR labels \n"
           f"par {model_name} prend {(end_time - start_time)/3600:.2f} hours !\n")
     
-    if log:# 中断无法储存？
-        today=datetime.today()
-        today_str=today.strftime('%Y-%m-%d %H:%M')
-        # 和df_zsc存在同一个地方
-        outpath_log=os.path.join(os.path.dirname(outpath_zsc_result), f"run.txt")
-                
-        with open(outpath_log, "a", encoding="utf-8") as f:
-            f.write(
-                f"{today_str.center(100, '-')}\n"
-                f"df_unique:{path_df_unique}\n"
-                f"zsc done :{len(results_list)}*{len(labels_en)}"
-                f"labels EN: {labels_en}\n"
-                f"labels FR :{labels_fr}\n"
-                f"model : {model_name}\n"
-                f"df_items :{outpath_zsc_result}\n"
-                f"runtime: {(end_time - start_time)/3600:.2f} hours\n"
-            )
-            print(f"✅ [SAVE] log saved to {outpath_log}!")
-    
-    return pd.DataFrame(results_list)
-
+    return results_df
 
 
 
